94d4f9e0b63381ffd40deed69d7ae69b28df5a4f
Sergeev Leonid <leonidsergeew@gmail.com>
2018-07-04 11:29:46 +0400

    eslint added

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    "extends": "airbnb",
+    "plugins": [
+        "react",
+        "jsx-a11y",
+        "import"
+    ],
+    "rules": {
+        "no-unused-vars": 0,
+        "no-underscore-dangle": 0,
+        "linebreak-style": 0,
+        "no-tabs": 0,
+        "react/jsx-filename-extension": 0,
+        "no-use-before-define": 0,
+        "react/destructuring-assignment": 1,
+        "react/jsx-no-bind": 1,
+        "react/prop-types": 0,
+        "func-names": 0,
+    }
+};
